8b76a6b4d872ffe4289c7492eba0742639e84a66
gavrun <38539655+gavrun@users.noreply.github.com>
2021-10-31 12:38:15 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace console_control_flow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //quit
+            bool quitProgram = false;
+
+            do
+            {
+                //message
+                Console.WriteLine("Input a task to run (rewards, comments, tasks) or exit (quit):");
+
+                //input actions
+                var selectedTask = Console.ReadLine();
+
+                //message
+                Console.WriteLine("Input a data set (ds1, ds2):");
+
+                //input data set
+                var selectedDataSet = Console.ReadLine();
+
+                //show results
+                var surveyResults = ; //TBD in a new class
+
+                switch (selectedTask)
+                {
+                    case "rewards":
+                        //action 1
+                        break;
+                    case "comments":
+                        //action 2
+                        break;
+                    case "tasks":
+                        //action3
+                        break;
+                    case "quit":
+                        //quit action
+                        break;
+                    default:
+                        //daction
+                        break;
+                }
+
+                //spacer
+                Console.WriteLine();
+            }
+            while (!quitProgram);
+        }
+    }
+}
---
+++ Results.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_control_flow
+{
+    class Results
+    {
+        //ratings
+
+        public double ServiceScore { get; set; }
+        public double CoffeeScore { get; set; }
+        public double PriceScore { get; set; }
+        public double FoodScore { get; set; }
+        public double WouldRecommend { get; set; }
+        public string FavoriteProduct { get; set; }
+        public string LeastFavoriteProduct { get; set; }
+        public string AreaToImprove { get; set; }
+
+        //couts
+
+        public double NumberSurveyed { get; set; }
+        public double NumberResponded { get; set; }
+        public double NumberRewardsMembers { get; set; }
+
+        //responces
+
+        public List<SurveyResponse> Responses { get; set; }
+    }
+}
